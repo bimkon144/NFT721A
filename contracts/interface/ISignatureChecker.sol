35b83d6f26f0ba19a1c1d38f7fd970c34345e0fc
bimkon <aleksei.kutsenko@metalamp.io>
2022-10-24 20:17:45 +0400

    refactoring code

--- contracts/interface/ISignatureChecker.sol
+++ contracts/interface/ISignatureChecker.sol
@@ -2,7 +2,5 @@
 pragma solidity 0.8.13;
 
 interface ISignatureChecker {
-
-  function isValidSignature(bytes calldata signature, address sender) external view returns (bool);
-
+    function isValidSignature(bytes calldata signature, address sender) external view returns (bool);
 }
